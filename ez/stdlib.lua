33170e4c5e122fc5a79d78be466d4973eb2ac399
Mikołaj Biel <mbiel@mailbox.org>
2018-12-23 00:54:48 +0100

    Add ez.stdlib.slice

--- ez/stdlib.lua
+++ ez/stdlib.lua
@@ -4,4 +4,5 @@ return {
 	    __newindex = function (t, k, v) setter(k, v) end
    }) end,
    extendtable = function (t1, t2) for k, v in pairs(t2) do table.insert(t1, v) end end,
+   slice = function (t, a, b) newt = {} for i=a,b do table.insert(newt, t[i]) end return newt end,
 }
