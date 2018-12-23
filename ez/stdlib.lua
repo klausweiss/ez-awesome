4aeda811432db29fd440df63ad0e30b2ce6defd4
Mikołaj Biel <mbiel@mailbox.org>
2018-12-23 14:25:57 +0100

    Add jointables to stdlib

--- ez/stdlib.lua
+++ ez/stdlib.lua
@@ -1,8 +1,19 @@
 return {
+   extendtable = function (t1, t2) for _, v in pairs(t2) do table.insert(t1, v) end
+   end,
+   jointables = function (...)
+      args = { ... }
+      local newtable = {}
+      for _, t in pairs(args) do
+	 for _, v in pairs(t) do
+	    table.insert(newtable, v)
+	 end
+      end
+      return newtable
+   end,
    noop = function (...) end,
    settertable = function (setter) return setmetatable({}, {
 	    __newindex = function (t, k, v) setter(k, v) end
    }) end,
-   extendtable = function (t1, t2) for _, v in pairs(t2) do table.insert(t1, v) end end,
    slice = function (t, a, b) newt = {} for i=a,b do table.insert(newt, t[i]) end return newt end,
 }
