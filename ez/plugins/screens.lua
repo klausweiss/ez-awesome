ef58c4365d3fe48e911e446d0b84105d5c66a0a0
Mikołaj Biel <mbiel@mailbox.org>
2018-12-29 20:51:54 +0100

    Export variables from screens plugin

--- ez/plugins/screens.lua
+++ ez/plugins/screens.lua
@@ -3,8 +3,13 @@ local awful = require("awful")
 
 return {
    getter = {
-      focus_left  = function () awful.screen.focus_bydirection("left",  awful.screen.focused()) end,
-      focus_right = function () awful.screen.focus_bydirection("right", awful.screen.focused()) end,
-      move_client = function (client_) c:move_to_screen() end,
+      focus_left_screen  = function () awful.screen.focus_bydirection("left",  awful.screen.focused()) end,
+      focus_right_screen = function () awful.screen.focus_bydirection("right", awful.screen.focused()) end,
+      move_client_to_another_screen = function () client.focus:move_to_screen() end,
+   },
+   export = {
+      "focus_left_screen",
+      "focus_right_screen",
+      "move_client_to_another_screen",
    }
 }
