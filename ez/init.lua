2044cf02b2415c826bb54afb9fa27b80dfeeccc8
Mikołaj Biel <mbiel@mailbox.org>
2018-12-29 21:18:39 +0100

    Autoload default plugins

--- ez/init.lua
+++ ez/init.lua
@@ -1,6 +1,25 @@
 local submodule = require("ez.submodule")
 local config_module = require("ez.config_module")
 
-local config_setter = submodule(config_module)
+local load_default_plugins = function(ez_)
+   -- prevent recursively calling this function
+   if __ez_started_loading_plugins then return end
+   __ez_started_loading_plugins = true
 
-return config_setter
+   local _
+   _ = ez_.client
+   _ = ez_.errors_handler
+   _ = ez_.keyboard
+   _ = ez_.layout
+   _ = ez_.mouse
+   _ = ez_.run
+   _ = ez_.screens
+   _ = ez_.tags
+   _ = ez_.wibar
+   _ = ez_.widgets
+end
+
+local ez = submodule(config_module)
+load_default_plugins(ez)
+
+return ez
