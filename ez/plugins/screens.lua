669aeb4b9d23057351abe7debb7619b04b6cf232
Mikołaj Biel <mbiel@mailbox.org>
2018-12-29 21:23:05 +0100

    Fix move_client_to_another_screen error when single screen

--- ez/plugins/screens.lua
+++ ez/plugins/screens.lua
@@ -5,7 +5,7 @@ return {
    getter = {
       focus_left_screen  = function () awful.screen.focus_bydirection("left",  awful.screen.focused()) end,
       focus_right_screen = function () awful.screen.focus_bydirection("right", awful.screen.focused()) end,
-      move_client_to_another_screen = function () client.focus:move_to_screen() end,
+      move_client_to_another_screen = function () if client.focus then client.focus:move_to_screen() end end,
    },
    export = {
       "focus_left_screen",
