3379e45cfbed151e59a62c8243c3eae2c278b123
Mikołaj Biel <mbiel@mailbox.org>
2018-12-30 12:51:30 +0100

    Update outer init to support using awesome-ez module as ez

--- init.lua
+++ init.lua
@@ -1,10 +1,14 @@
 -- add /ez/ subdirectory to the library load path
-local _, this_path = ...
-local dir_path = this_path:match(".*/")
-package.path = (";"
-		   .. package.path .. ";"
-		   .. dir_path .. "?.lua;"
-		   .. dir_path .. "ez/init.lua;"
-	       )
+local this_name, this_path = ...
 
-return require("ez")
+if not __ez_modified_path then
+    local dir_path = this_path:match(".*/")
+    package.path = (";"
+               .. package.path .. ";"
+               .. dir_path .. "?.lua;"
+               .. dir_path .. "ez/init.lua;"
+               )
+    __ez_modified_path = true
+end
+
+return require(this_name .. ".ez")
