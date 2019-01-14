87137caab9f1fa36f2e2ea6f9134287d4596923c
Mikołaj Biel <mbiel@mailbox.org>
2019-01-14 14:26:49 +0100

    [theme] Add setting gaps

--- ez/plugins/theme.lua
+++ ez/plugins/theme.lua
@@ -4,12 +4,30 @@ local gears     = require("gears")
 local stdlib    = require("ez.stdlib")
 
 local config = {
+   gaps      = nil,
    theme     = gears.filesystem.get_themes_dir() .. "default/theme.lua",
    wallpaper = nil,
 }
 
 local init_theme = function ()
    beautiful.init(config.theme)
+end
+
+local init_gaps = function ()
+   if not config.gaps then return end
+
+   local client_rules = {
+      {
+	 rule_any = {
+	    type = {"normal", "dialog"},
+	 },
+	 properties = {
+	    size_hints_honor = false,
+	 },
+      }
+   }
+   beautiful.useless_gap = config.gaps
+   stdlib.extendtable(awful.rules.rules, client_rules)
 end
 
 local init_wallpaper = function ()
@@ -32,6 +50,7 @@ local path_config_setter = function(property_name)
 end
 
 local setter = {
+   gaps      = config_setter("gaps"),
    theme     = path_config_setter("theme"),
    wallpaper = path_config_setter("wallpaper"),
 }
@@ -41,6 +60,7 @@ local setters = function (key, value) return setter[key](value) end
 local setup = function ()
    init_theme()
 
+   init_gaps()
    init_wallpaper()
 end
 
