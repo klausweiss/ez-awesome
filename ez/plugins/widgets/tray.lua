c3ab026690afb55f68bafa5e816c02f72f6a8bed
Mikołaj Biel <mbiel@mailbox.org>
2019-02-18 19:19:32 +0100

    [widgets] Show tray only on one random screen

--- ez/plugins/widgets/tray.lua
+++ ez/plugins/widgets/tray.lua
@@ -4,6 +4,8 @@ local common = require("ez.plugins.widgets.common")
 
 
 return function (_screen)
+   if ez_tray_visible then return nil else ez_tray_visible = true end
+
    return common.mkwidget(
       common.texticon_box("",
 			  wibox.widget.systray()
