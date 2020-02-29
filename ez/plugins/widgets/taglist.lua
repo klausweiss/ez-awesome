84212f578f07954fbdee41d99cdbd0c0bba47b62
Mikołaj Biel <mbiel@mailbox.org>
2020-02-29 20:36:20 +0100

    [taglist] Use custom widget, add margins

--- ez/plugins/widgets/taglist.lua
+++ ez/plugins/widgets/taglist.lua
@@ -1,4 +1,6 @@
-local awful     = require("awful")
+local awful = require("awful")
+local gears = require("gears")
+local wibox = require("wibox")
 
 local stdlib  = require("ez.stdlib")
 local ez      = require("ez")
@@ -7,13 +9,34 @@ local tags    = ez.tags
 
 
 return function (screen)
-    local taglist_buttons = stdlib.jointables(
-	awful.button({}, mouse._buttons_numbers.left_click,  tags.show_only_tag),
-	awful.button({}, mouse._buttons_numbers.right_click, tags.toggle_tag),
-	awful.button({}, mouse._buttons_numbers.wheel_up,    tags.show_previous_tag),
-	awful.button({}, mouse._buttons_numbers.wheel_down,  tags.show_next_tag)
-    )
-    return awful.widget.taglist(screen,
-				awful.widget.taglist.filter.all,
-				taglist_buttons)
+   local taglist_buttons = stdlib.jointables(
+      awful.button({}, mouse._buttons_numbers.left_click,  tags.show_only_tag),
+      awful.button({}, mouse._buttons_numbers.right_click, tags.toggle_tag),
+      awful.button({}, mouse._buttons_numbers.wheel_up,    tags.show_previous_tag),
+      awful.button({}, mouse._buttons_numbers.wheel_down,  tags.show_next_tag)
+   )
+   taglist_args = {
+      screen = screen,
+      filter = awful.widget.taglist.filter.all,
+      buttons = taglist_buttons,
+      widget_template = {
+	 {
+	    {
+	       {
+		  id = "text_role",
+		  widget = wibox.widget.textbox,
+	       },
+	       layout = wibox.layout.fixed.horizontal
+	    },
+	    left = 18,
+	    right = 18,
+	    top = 5,
+	    bottom = 5,
+	    widget = wibox.container.margin
+	 },
+	 id  = 'background_role',
+	 widget = wibox.container.background,
+      }
+   }
+   return awful.widget.taglist(taglist_args)
 end
